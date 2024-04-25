10f6869cb26c120993ac98bd49e107945c0200b5
alaeddineth-atsudev <167719767+alaeddineth-atsudev@users.noreply.github.com>
2024-04-25 15:32:46 +0100

    "read_and_searching"

--- app/Livewire/ToDoList.php
+++ app/Livewire/ToDoList.php
@@ -4,12 +4,15 @@ namespace App\Livewire;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 use App\Models\Todo;
+use Livewire\WithPagination;
 
 class ToDoList extends Component
 {
     
+    use WithPagination;
     #[Validate('required|min:3|max:50', )]
     public $name ; 
+    public $search;
     public function create(){
         $validated = $this->validateOnly('name');
         Todo::create($validated);
@@ -20,7 +23,9 @@ class ToDoList extends Component
     }
     public function render()
     {
-        return view('livewire.to-do-list');
+        
+        return view('livewire.to-do-list',
+        ['todos' => Todo::latest()->where('name','like', "%{$this->search}%")->paginate(5)]);
 
     }
 }
